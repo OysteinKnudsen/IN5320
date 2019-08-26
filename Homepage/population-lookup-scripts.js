071f851585e314a6fb2d06141ba6893b13fcb0e5
OysteinKnudsen <oysteinknudsen96@gmail.com>
2019-08-26 13:14:04 +0200

    delete button

--- Homepage/population-lookup-scripts.js
+++ Homepage/population-lookup-scripts.js
@@ -1,9 +1,26 @@
 
 
 function appendToCountryList(name){
+    // Create the list item
 var list = document.getElementById("countryList");
 var newListItem = document.createElement("li");
+
+// Add the country name and delete button 
 newListItem.appendChild(document.createTextNode(name));
+var delBtn = document.createElement("button");
+delBtn.innerHTML = "X";
+delBtn.onclick = function() {
+    delBtn.parentElement.remove();
+};
+
+newListItem.appendChild(delBtn);
 list.appendChild(newListItem);
+
+
+//Clear the input field and give focus
+var countryInputField = document.getElementById('countryInputField');
+countryInputField.value = '';
+countryInputField.focus();
 }
+
 
