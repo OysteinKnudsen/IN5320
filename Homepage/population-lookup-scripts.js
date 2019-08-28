f95db9333614c16da908b801a9de5bb85520371b
OysteinKnudsen <oysteinknudsen96@gmail.com>
2019-08-28 15:02:37 +0200

    fix to population lookup

--- Homepage/population-lookup-scripts.js
+++ Homepage/population-lookup-scripts.js
@@ -1,83 +1,87 @@
 window.onload = populateCountryList;
 
-async function appendToCountryList(name) {
-  let population = await getPopulation(name);
+/**
+ * Appends a country to the list of countries
+ * @param {*} country Country to be added.
+ * @param {*} store Optional boolean to decide if the country should be stored in local memory
+ */
+async function appendToCountryList(country, store) {
+  var countryEntry = country;
 
-  console.log("tråd fortsetter");
+  if (!endsWithNumbers(country)) {
+    let countryPopulation = await getPopulation(country);
+    countryEntry = `${country} - ${countryPopulation}`;
+  }
 
   // Create the list item
   let list = document.getElementById("countryList");
-  var newListItem = document.createElement("li");
+  let newListItem = document.createElement("li");
 
   // Add the country name and delete button
-  let countryEntry = `${name} - ${population}`;
+
   newListItem.appendChild(document.createTextNode(countryEntry));
 
-  var delBtn = document.createElement("button");
+  //Create deletebutton with onlick function
+  let delBtn = document.createElement("button");
   delBtn.innerHTML = "X";
   delBtn.setAttribute("onclick", "deleteCountryFromList(this)");
 
   newListItem.appendChild(delBtn);
   list.appendChild(newListItem);
 
-  appendToLocalStorage(countryEntry);
+  if (store) {
+    appendToLocalStorage(countryEntry);
+  }
 
   //Clear the input field and give focus
-  var countryInputField = document.getElementById("countryInputField");
+  let countryInputField = document.getElementById("countryInputField");
   countryInputField.value = "";
   countryInputField.focus();
 }
 
+/**
+ * Appends a country to the local storage.
+ */
 function appendToLocalStorage(newCountry) {
-  var existingCountries = JSON.parse(localStorage.getItem("countries"));
-  if (existingCountries == null) existingCountries = [];
+  let existingCountries = localStorage.getItem("countries");
+
+  if (!existingCountries) existingCountries = [];
+
+  existingCountries = JSON.parse(existingCountries);
+
   existingCountries.push(newCountry);
   localStorage.setItem("countries", JSON.stringify(existingCountries));
 }
 
-//This is a duplicate function almost, should refactor to remove redundant code.
-function appendToCountryListNoStorage(name) {
-  // Create the list item
-  let list = document.getElementById("countryList");
-  var newListItem = document.createElement("li");
-
-  // Add the country name and delete button
-  newListItem.appendChild(document.createTextNode(name));
-
-  var delBtn = document.createElement("button");
-  delBtn.innerHTML = "X";
-  delBtn.setAttribute("onclick", "deleteCountryFromList(this)");
-
-  newListItem.appendChild(delBtn);
-  list.appendChild(newListItem);
-
-  //Clear the input field and give focus
-  var countryInputField = document.getElementById("countryInputField");
-  countryInputField.value = "";
-  countryInputField.focus();
-}
-
+/**
+ * Populates the list of countries with values from the local storage.
+ */
 function populateCountryList() {
-  var existingCountries = JSON.parse(localStorage.getItem("countries"));
+  let existingCountries = localStorage.getItem("countries");
 
-  if (existingCountries == null) existingCountries = [];
+  if (!existingCountries) existingCountries = [];
+
+  existingCountries = JSON.parse(existingCountries);
 
   for (let i = 0; i < existingCountries.length; i++) {
     let country = existingCountries[i];
-    appendToCountryListNoStorage(country);
+    appendToCountryList(country, false);
   }
 }
 
+/**
+ * Deletes a country from the list of countries and also deletes from local storage
+ */
 function deleteCountryFromList(element) {
   // Get the name of the country which is to be deleted
-  var countryName = element.parentElement.firstChild.nodeValue;
+  let countryEntry = element.parentElement.firstChild.nodeValue;
 
   element.parentElement.remove();
 
   //Delete the country from the local storage
-  var allCountries = JSON.parse(localStorage.getItem("countries"));
+  let allCountries = JSON.parse(localStorage.getItem("countries"));
 
-  var countriesAfterDelete = allCountries.filter(ele => ele != countryName);
+  let countriesAfterDelete = allCountries.filter(ele => ele != countryEntry);
 
   localStorage.setItem("countries", JSON.stringify(countriesAfterDelete));
 }
@@ -99,7 +103,10 @@ function search(list, searchWord) {
   let resultList = [];
 
   list.forEach(element => {
-    if (startsWith(element.toLowerCase(), searchWord.toLowerCase())) {
+    if (
+      startsWith(element.toLowerCase(), searchWord.toLowerCase()) &&
+      searchWord.length <= element.length
+    ) {
       resultList.push(element);
     }
   });
@@ -109,26 +116,38 @@ function search(list, searchWord) {
 
 function clearCountryList() {
   const countryList = document.getElementById("countryList");
-
   countryList.innerHTML = "";
 }
 
 function createList(countries) {
   clearCountryList();
-
   countries.forEach(element => {
-    appendToCountryListNoStorage(element);
+    appendToCountryList(element, false);
   });
 
   document.getElementById("searchBar").focus();
 }
 
+/**
+ * Calls population API to get the population of a country today.
+ */
 async function getPopulation(country) {
-  let url = `http://54.72.28.201/1.0/population/${country}/2019-08-26`;
+  //Get current date
+  let today = new Date();
+  let date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+
+  // build URL for api call
+  let url = `http://54.72.28.201/1.0/population/${country}/${date}`;
+
+  //Call API to get population
   let response = await fetch(url);
-  console.log("henterdata");
   let json = await response.json();
 
-  console.log("json", json.total_population.population);
   return json.total_population.population;
+}
+
+function endsWithNumbers(string) {
+  var regex = /([a-zA-Z]*[0-9])$/;
+  return string.match(regex);
 }
